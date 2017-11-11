107747bffd01e440157b2783bb620c4a4a890a40
majaosinska <majaosinska@o2.pl>
2017-11-11 19:22:47 +0100

    Add project files.

---
+++ Równanie Pitagorasa/Calculations.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Równanie_Pitagorasa
+{
+    public static class Calculations
+    {
+        public static void compareArguments()
+        {
+
+
+            int a, b, c;
+
+            Console.WriteLine("Podaj wartość a");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Podaj wartość b");
+            int.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Podaj wartość c");
+            int.TryParse(Console.ReadLine(), out c);
+            var result = (a * a + b * b) == c * c;
+            if (result == true)
+            {
+                Console.WriteLine(result);
+                Console.WriteLine("Podane liczby tworzą trójkę pitagorejską");
+            }
+            else
+            {
+                Console.WriteLine("Podane liczby nie tworzą trójki pitagorejskiej");
+            }
+
+            Console.WriteLine("\n\n");
+            Console.ReadKey();
+        }
+
+
+        public static void calculateC()
+        {
+            Console.WriteLine("Wyliczmy równanie z niewiadomą c");
+            int a, b, c;
+
+            Console.WriteLine("Podaj wartość a");
+            int.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Podaj wartość b");
+            int.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Wynik niewiadomej c wynosi:");
+            Console.WriteLine(Math.Sqrt(a * a + b * b));
+
+            Console.ReadKey();          
+        }
+
+        public static void calculateA()
+        {
+            Console.WriteLine("Wyliczmy równanie z niewiadomą a");
+            int a, b, c;
+
+            Console.WriteLine("Podaj wartość b");
+            int.TryParse(Console.ReadLine(), out b);
+            Console.WriteLine("Podaj wartość c (wynik)");
+            int.TryParse(Console.ReadLine(), out c);
+            Console.WriteLine("Wynik niewiadomej a wynosi:");
+            Console.WriteLine(Math.Sqrt(c* c - b* b));
+
+            Console.ReadKey();
+        }
+ }
+}
---
+++ Równanie Pitagorasa/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Równanie_Pitagorasa;
+
+namespace Równanie_Pitagorasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int chosedOption;
+
+
+            Console.WriteLine("Które z obliczeń z równania Pitagorasa chcesz wykonać:\n"+ 
+                "1. Chcesz sprawdzić czy liczby podane przez Ciebie tworzą równanie?\n"+ 
+                "2. Chcesz obliczyć wynik dla podanych wartości?\n"+ 
+                "3. Chcesz wyliczyć niewiadomą a?\n");
+            //Console.WriteLine();
+            int.TryParse(Console.ReadLine(), out chosedOption);
+            
+            switch (chosedOption)
+            {
+                case 1:
+                  
+                Calculations.compareArguments();
+                break;                                           
+                    
+                case 2:
+
+                Calculations.calculateC();
+                break;
+
+                case 3:
+
+                Calculations.calculateA();
+                break;
+
+            }
+
+            
+
+              
+               
+
+            
+
+        }
+
+    }
+}
